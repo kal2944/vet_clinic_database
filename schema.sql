3d65b693eb131922d8edbf2307314f6c8cf119ae
kdan2944 <kdan2944gmail.com>
2023-03-29 04:25:38 +0300

    Create Table

--- schema.sql
+++ schema.sql
@@ -2,4 +2,11 @@
 
 CREATE TABLE animals (
     name varchar(100)
+id               INT GENERATED ALWAYS AS IDENTITY,
+name             VARCHAR(250),
+data_of_birth    DATE,
+escape_attempts  INT,
+neutered         BOOLEAN,
+weight_kg        DECIMAL,
+PRIMARY KEY(id)
 );
