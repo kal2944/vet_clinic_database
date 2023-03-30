fd46a54b00d5139adcee488f5dc2459a6b6a14db
kdan2944 <kdan2944gmail.com>
2023-03-30 19:31:51 +0300

    To answer who owns with  the most animals

--- queries.sql
+++ queries.sql
@@ -59,3 +59,5 @@ FROM animals INNER JOIN owners ON  animals.owner_id = owners.id INNER JOIN speci
 SELECT full_name AS "OWNER NAME" , animals.name AS "ANIMALS NAME",species.name AS "Animal Species"
 FROM animals INNER JOIN owners ON  animals.owner_id = owners.id INNER JOIN species ON  animals.species_id = species.id WHERE owners.id = 5 and animals.escape_attempts = 0;  
 
+SELECT full_name AS "OWNER NAME",  COUNT(animals.name) AS "ANIMAL COUNT" 
+FROM owners JOIN animals on owners.id = animals.owner_id GROUP By full_name ORDER BY COUNT(animals.name) DESC LIMIT 1;
